36c0d6909b62a09fd2ec1bcfe1f070f68dddc517
minhngoc01221 <ngocnguyenminh779@gmail.com>
2026-03-10 23:28:59 +0700

    connetdatabase

---
+++ app/lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/project_travel';
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  datasourceUrl: databaseUrl,
+});
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
